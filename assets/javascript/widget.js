7c40f4167734e1512be374e800fb58de19a1c0a5
Logan <logan.volkers@yupiq.com>
2014-08-12 22:13:47 +0000

    LV: Added js imports

---
+++ assets/javascript/widget.js
@@ -0,0 +1,15 @@
+"use strict";
+
+$(document).ready(function(){
+
+    $('.small-avatar').popover(
+      {
+        trigger: 'hover',
+        placement: 'top',
+        html: 'true',
+        content: function(el){
+            return $(el).find(".hover").html();
+        }
+    });
+
+});
